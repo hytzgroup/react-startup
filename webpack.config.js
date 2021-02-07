cfb1379758a56f823c233f39be5f712496f47dbc
thomas-kala <yangtao080304@163.com>
2021-02-07 14:08:59 +0800

    开发环境

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 module.exports = {
+  mode: "development",
   entry: {
     app: "./src/index.js",
     print: "./src/print.js",
   },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: '/'// webpack-dev-middleware时使用
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new WebpackManifestPlugin({}),
-    new HtmlWebpackPlugin({
-      title: "管理输出",
-    }),
-    
+    new HtmlWebpackPlugin(),
   ],
 };

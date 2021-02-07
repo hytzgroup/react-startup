6bf546de37633c6d9287b46184f7b913d4c4a48d
thomas-kala <yangtao080304@163.com>
2021-02-07 15:29:16 +0800

    hmr

--- src/print.js
+++ src/print.js
@@ -1,4 +1,4 @@
 export default function printMe() {
-  console.log("I get called from print.js!wwwwwww");
+  console.log("I get called from print.js!ppppp");
   // cosnole.log('I get called from print.js!');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,7 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "/", // webpack-dev-middleware时使用
+    path: path.resolve(__dirname, "./dist")
   },
   plugins: [
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
